73efe1aa5fa7a83699817d140e7df9bba741866b
pablo <pablo.alejandro315@gmail.com>
2023-01-29 04:40:18 -0300

    Portfolio

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-    "https://raw.githubusercontent.com/PabloGonzalez315/portfolio/master/src/Assets/CVPabloGonzalez1122.pdf";
+    "https://raw.githubusercontent.com/PabloGonzalez315/portfolio/master/src/assets/CVPabloGonzalez1122.pdf";
 
 function ResumeNew() {
     const [width, setWidth] = useState(1200);
